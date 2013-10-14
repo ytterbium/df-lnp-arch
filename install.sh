798d18ad20f574e4ef0ecb6a004fad817e8e3da6
Yann Balland <ytterbium@laposte.net>
2013-10-14 17:42:26 +0400

    Install script OK

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
+DEFAULT_INSTALL="bin/Dwarf Fortress3" # Will hahe a trailing $HOME
 users_valid=false
 while [ $users_valid != "true" ]; do
     echo ''
-    echo -n "For which users install df-lnp ? "
+    echo  "For which users install df-lnp ? "
 
     read users
 
@@ -10,7 +11,26 @@ while [ $users_valid != "true" ]; do
     for user in $users; do
         if [ ! -d "/home/$user" ]
         then users_valid=false
+            echo "$user is not a valid user"
         fi
     done
+done
+echo -n ""
+# Determine where should we install df
+for user in $users; do
+    INSTALL_DIR="/home/$user/$DEFAULT_INSTALL"
+
+    echo -n "For $user, where should Dwarf Fortress be installed? [$INSTALL_DIR]: "
+    read PREFERRED_DIR
+    
+    # If the user entered a preferred directory, use that,
+    # otherwise use the install directory.
+    if [ -n "$PREFERRED_DIR" ]; then
+    # Use sed and custom ; delimeter to replace the first instance of ~ with the user's home directory.
+        INSTALL_DIR=$(echo "$PREFERRED_DIR" | sed "s;~;$HOME;")
+    fi
+
+    su $user -c "cp -rn /opt/df-lnp-arch/ \"$INSTALL_DIR\""
+
 done
 
